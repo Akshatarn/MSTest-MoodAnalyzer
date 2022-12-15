97a4253f67631f2b579393d33aa20279c06386a4
akshata_rn <akshatarn123@gamail.com>
2022-12-15 19:03:23 +0530

    Custom Exception If empty or null

---
+++ MoodAnalyserProject/MoodAnalyserException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProject
+{
+    public class MoodAnalyserException : Exception  //created Custom exception , requirement for 3rd UC .
+    {
+        public ExceptionTypes exceptionType;
+        public enum ExceptionTypes
+        {
+            NULL_MESSAGE,
+            EMPTY_MESSAGE
+        }
+        public MoodAnalyserException(string msg, ExceptionTypes exceptionType) : base(msg)
+        {
+            this.exceptionType = exceptionType;
+        }
+    }
+}
