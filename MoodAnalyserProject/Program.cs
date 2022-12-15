6c85963fd088b6da3dcc43b78463eb49f8e65e46
akshata_rn <akshatarn123@gamail.com>
2022-12-15 18:54:57 +0530

    Ability to analyse Happy or Sad Mood

---
+++ MoodAnalyserProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MSTesting is been done in this project on Mood Analyser including Exceptions and Reflection .");
+        }
+    }
+}
